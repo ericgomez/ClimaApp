301dc8371804897656245e321db5906853e418a6
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-08-05 15:51:43 -0500

    Modelo para controlar la aplicación

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const { readInput, inquirerMenu, pause } = require('./helpers/inquirer');
+const Searchs = require('./models/searchs');
 
 console.clear();
 
 const main = async () => {
+  const searchs = new Searchs();
+
   do {
     opt = await inquirerMenu();
 
     switch (opt) {
-      case '1':
+      case 1:
+        // Mostrar mensaje
+        const place = await readInput('City: ');
+        console.log(place);
+        // Buscar los lugares
+
+        // Selecionar lugares
+
+        // Clima
+
+        // Mostrar resultados
+        console.log('\nInformation city'.green);
+        console.log('City:');
+        console.log('Lat:');
+        console.log('Lng:');
+        console.log('Temperature:');
+        console.log('Minimum:');
+        console.log('Maximum:');
         break;
-      case '2':
+      case 2:
         break;
     }
     if (opt !== 0) await pause();
---
+++ models/searchs.js
@@ -0,0 +1,16 @@
+class Searchs {
+  record = ['Tegucigalpa', 'Madrid', 'San José'];
+
+  constructor() {
+    // TODO: Leer DB si existe
+  }
+
+  async city(place = '') {
+    // Peticion HTTP
+    console.log(place);
+
+    return []; //Retornar los lugares
+  }
+}
+
+module.exports = Searchs;
